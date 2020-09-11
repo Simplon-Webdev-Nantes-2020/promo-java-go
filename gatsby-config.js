1c38f2db594e1ec844f5f52e9bf02ce7906837d0
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-09-11 11:54:56 +0200

    Gestion des images dans les tuto md

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,28 @@ module.exports = {
     plugins: [
         `gatsby-plugin-react-helmet`,
         `gatsby-source-data`,
-        `gatsby-transformer-remark`,
+        `gatsby-remark-images`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+              plugins: [
+                {
+                  resolve: `gatsby-remark-images`,
+                  options: {
+                    maxWidth: 800,
+                  },
+                },
+              ],
+            },
+        },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `pages`,
                 path: `${__dirname}/src/pages`
-            }
+            },
         },
         {
             resolve: `gatsby-plugin-sass`,
