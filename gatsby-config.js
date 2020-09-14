d076e427dc1d9904d1b79d88954739d4d531997c
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-09-14 18:01:19 +0200

    simplification sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
         `gatsby-remark-images`,
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
+        `gatsby-plugin-sass`,
         {
             resolve: `gatsby-transformer-remark`,
             options: {
@@ -36,40 +37,6 @@ module.exports = {
                 name: `pages`,
                 path: `${__dirname}/src/pages`
             },
-        },
-        {
-            resolve: `gatsby-plugin-sass`,
-            options: {
-                functions: {
-                    "getPaletteKey($key)": function(sassKey) {
-                        function hexToRgb(hex) {
-                            // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-                            let shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-                            hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-                                return r + r + g + g + b + b;
-                            });
-
-                            let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-                            return result ? {
-                                r: parseInt(result[1], 16),
-                                g: parseInt(result[2], 16),
-                                b: parseInt(result[3], 16)
-                            } : null;
-                        }
-                        let sassParams = siteMetadata.palettes[siteMetadata.palette].sass;
-                        let key = sassKey.getValue();
-                        let value = sassParams[key];
-                        let colorRegExp = /^#(?:[a-f\d]{3}){1,2}$/i;
-                        let result;
-                        if (colorRegExp.test(value)) {
-                            result = hexToRgb(value);
-                            result = new sass.types.Color(result.r, result.g, result.b);
-                        } else {
-                            result = sassUtils.castToSass(value)
-                        }
-                        return result;
-                    }
-                }}
         },
         {
             resolve: `gatsby-remark-page-creator`,
